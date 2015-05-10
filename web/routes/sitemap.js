7eeddbeaf21815cb0da5cf2e371081004233f65c
Ian Webster <ianw_git@ianww.com>
2015-05-10 11:13:23 -0700

    remove slash prefix in sitemap

--- web/routes/sitemap.js
+++ web/routes/sitemap.js
@@ -3,18 +3,18 @@ var dinomap = require('./dinomap.js');
 
 var sitemap_urls = [];
 sitemap_urls.push({url: '', changefreq: 'daily', priority: 1.0});
-sitemap_urls.push({url: '/triassic-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/jurassic-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/cretaceous-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/north-america-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/south-america-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/europe-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/africa-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/madagascar-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/asia-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/india-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/australia-dinosaurs', changefreq: 'weekly', priority: 0.9});
-sitemap_urls.push({url: '/antarctica-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'triassic-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'jurassic-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'cretaceous-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'north-america-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'south-america-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'europe-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'africa-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'madagascar-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'asia-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'india-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'australia-dinosaurs', changefreq: 'weekly', priority: 0.9});
+sitemap_urls.push({url: 'antarctica-dinosaurs', changefreq: 'weekly', priority: 0.9});
 for (var key in dinomap.get()) {
   sitemap_urls.push({url: key + '-dinosaur-pictures', changefreq: 'weekly', priority: 0.8});
 }
