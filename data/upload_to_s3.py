48b96938a2829e5ea53f076edfae4a48161a52e2
Ian Webster <typppo@gmail.com>
2015-06-10 22:41:33 -0700

    processing improvements

--- data/upload_to_s3.py
+++ data/upload_to_s3.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import requests
 import secrets
 from urlparse import urlparse
 from os.path import splitext, basename
@@ -8,8 +7,9 @@ import cStringIO as StringIO
 import json
 import sys
 import boto
-from boto.s3.key import Key
+import hashlib
 
+from boto.s3.key import Key
 from PIL import Image
 
 conn = boto.connect_s3()
@@ -18,8 +18,17 @@ bucket.set_acl('public-read')
 
 MAX_WIDTH = 900
 
+def compute_tag(url):
+    return hashlib.sha1(url.encode('utf-8')).hexdigest()
+
 def key_from_url(url):
-    return '/images/' + urllib.quote(urllib.unquote(url)).replace('/', '_')
+    tag = compute_tag(url)
+    disassembled = urlparse(url)
+    filename, ext = splitext(basename(disassmebled.path))
+
+    return '/images/%s_%s%s' % (filename, tag, ext)
+
+    #return '/images/' + urllib.quote(urllib.unquote(url)).replace('/', '_')
 
 def upload(url):
     try:
@@ -41,11 +50,12 @@ def upload(url):
             im.resize(new_width, new_height)
         im.save(uploadfp, 'JPEG')
 
+        print 'Uploading', url, '\n\tto', name
+
         k.set_contents_from_file(fp)
         k.set_acl('public-read')
-        return "Success"
     except Exception, e:
-        return e
+        print e
 
 f = open('processed_all_cloudinary.json', 'r')
 dinos = json.loads(f.read())
@@ -54,5 +64,4 @@ f.close()
 for dino, dinostuff in dinos.iteritems():
     for image in dinostuff['images']:
         url = image['url'].encode('utf-8')
-        print 'Uploading', url
-        print upload(url)
+        upload(url)
