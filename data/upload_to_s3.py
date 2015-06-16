90901384543aedec67bdc3f19ef2fbb27d643eb1
Ian Webster <ianw_git@ianww.com>
2015-06-16 10:35:09 -0700

    add s3 urls and processed_s3.json

--- data/upload_to_s3.py
+++ data/upload_to_s3.py
@@ -45,9 +45,10 @@ def upload(url):
     try:
         url = url.encode('utf-8')
         name = key_from_url(url)
+        s3_url = 'https://s3.amazonaws.com/dinosaur-pictures/' + name
         if bucket.get_key(name):
-            print 'Already exists!'
-            return
+            #print 'Already exists!'
+            return s3_url
         k = Key(bucket)
         k.key = name
         file_object = urllib2.urlopen(url, timeout=30)           # 'Like' a file object
@@ -61,23 +62,53 @@ def upload(url):
             print 'Resizing image', width, height, 'to', new_width, new_height
             im = im.resize((new_width, new_height))
         uploadfp = StringIO.StringIO()
-        im.convert('RGB').save(uploadfp, 'JPEG')
+
+        # Preserve transparency if the image is in Pallette (P) mode.
+        transparency_formats = ('PNG', 'GIF', )
+        kwargs = {}
+        if im.mode == 'P' and format in transparency_formats:
+            kwargs['transparency'] = len(im.split()[-1].getcolors())
+        else:
+            im = im.convert('RGB')
+
+        im.save(uploadfp, 'JPEG', **kwargs)
         uploadfp.seek(0)
 
         print 'Uploading', url, '\n\tto', name
 
         k.set_contents_from_file(uploadfp)
         k.set_acl('public-read')
+        return s3_url
     except Exception, e:
         f = open('uploadserrors', 'a')
         f.write('%s %s\n' % (url, str(e)))
         print e
+        return ''
 
-f = open('processed_all_cloudinary.json', 'r')
+f = open('megascrape/processed_all.json', 'r')
 dinos = json.loads(f.read())
 f.close()
 
+blacklisted_urls = set()
+f = open('../web/reported.txt', 'r')
+blacklisted = f.readlines()
+f.close()
+for line in blacklisted:
+    idx = line.find(':')
+    url = line[idx+1:].strip()
+    dino = line[:idx]
+    blacklisted_urls.add(url)
+
+output = {}
 for dino, dinostuff in dinos.iteritems():
     for image in dinostuff['images']:
         url = image['url'].encode('utf-8')
-        upload(url)
+        if url in blacklisted_urls:
+            print 'Skipping blacklisted url', url
+            continue
+        uploaded_url = upload(url)
+        output[dino] = dinostuff
+        output[dino]['s3_url'] = uploaded_url
+f = open('processed_s3.json', 'w')
+f.write(json.dumps(output, indent=2))
+f.close()
