5cd37cf8f5d555007ef8f81d6b4cd6b2206061a6
Ian Webster <typppo@gmail.com>
2015-07-13 15:35:55 -0700

    Add refs to dinos flatfile from paleodb.

--- data/process_paleodb.py
+++ data/process_paleodb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import json
+import os
 import urllib2
 
 f = open('processed_s3.json', 'r')
@@ -10,15 +11,25 @@ c = 0
 for dino, dinostuff in dinos.iteritems():
     c += 1
     print 'Processing %s (%d/%d)' % (dino, c, len(dinos))
-    resp = urllib2.urlopen('https://paleobiodb.org/data1.1/colls/list.json?base_name=%s&show=ref,loc,time' % dino).read()
-    f = open('paleobiodb_list_colls_cache/%s' % dino, 'w')
-    f.write(resp)
-    f.close()
+
+    filepath = 'paleobiodb_list_colls_cache/%s' % dino
+    if os.path.isfile(filepath):
+        f = open(filepath, 'r')
+        resp = f.read()
+        f.close()
+    else:
+        resp = urllib2.urlopen('https://paleobiodb.org/data1.1/colls/list.json?base_name=%s&show=ref,loc,time' % dino).read()
+        f = open(filepath, 'w')
+        f.write(resp)
+        f.close()
 
     latlngs = []
+    refs = []
     for record in json.loads(resp)['records']:
         latlngs.append((record['lat'], record['lng']))
+        refs.append(record['ref'])
     dinostuff['fossil_latlngs'] = latlngs
+    dinostuff['refs'] = refs
 
 f = open('processed_s3_with_latlng.json', 'w')
 f.write(json.dumps(dinos, indent=2))
